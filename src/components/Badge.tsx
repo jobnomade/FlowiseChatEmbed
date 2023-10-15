c6b5871241c15a99cfe5828bb97c7e98df16d834
Nhan Vu <trinhanvu@gmail.com>
2023-10-15 22:07:46 +0200

    remove flowise, add ds_store to gitignore

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -61,7 +61,7 @@ export const Badge = (props: Props) => {
           id="lite-badge"
           style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Ainfach.ai & Flowise</span>
+        <span> Ainfach.ai</span>
       </a>
     </span>
   )
